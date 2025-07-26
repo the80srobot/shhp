88fffe04b5d8132c218dde81cea5ed3aacc38990
Adam Sindelar <adam@wowsignal.io>
2025-07-26 22:07:03 +0200

    Take damage when commands fail

--- functions.sh
+++ functions.sh
@@ -9,16 +9,23 @@ SHHP_BLINK="$(tput blink)"
 SHHP_RST="$(tput sgr0)"
 SHHP_HEART="♥"
 SHHP_COLOR=1
+export SHHP_TEMP="$(mktemp -d /tmp/shhp.XXXXXX)"
+trap 'rm -rf "${SHHP_TEMP}"' EXIT
 
-# Returns the current HP as an integer.
-# If SHHP_HP is not set, it defaults to SHHP_MAX_HP.
+# Returns the current HP as an integer. If hitpoints are not set, initializes to
+# SHHP_MAX_HP.
 #
-# Usage: shhp_hp
+# If an argument is passed, HP is set to that value and then returned.
+#
+# Usage: shhp_hp [HP]
 function shhp_hp() {
-    if [[ -z "${SHHP_HP}" ]]; then
-        SHHP_HP="${SHHP_MAX_HP}"
+    if (( $# > 0 )); then
+        echo "$1" > "${SHHP_TEMP}/hp"
     fi
-    printf "%d" "${SHHP_HP}"
+    if [[ ! -f "${SHHP_TEMP}/hp" ]]; then
+        echo "${SHHP_MAX_HP}" > "${SHHP_TEMP}/hp"
+    fi
+    cat "${SHHP_TEMP}/hp" | xargs
 }
 
 # Outputs a tput color code for the specified color. If SHHP_COLOR is unset,
@@ -93,10 +100,18 @@ function shhp_heal() {
         amount="$1"
     fi
 
-    if (( $(shhp_hp) + amount > SHHP_MAX_HP )); then
-        amount=$(( SHHP_MAX_HP - $(shhp_hp) ))
+    if (( $(shhp_hp) >= SHHP_MAX_HP )); then
+        return 0  # Already at max HP, no healing needed.
     fi
-    SHHP_HP=$(( $(shhp_hp) + amount ))
+
+    # If healing would exceed max HP, set to max HP.
+    # Otherwise, heal by the specified amount.
+    if (( $(shhp_hp) + amount > SHHP_MAX_HP )); then
+        shhp_hp $SHHP_MAX_HP > /dev/null
+    else
+        # Heal by the specified amount.
+        shhp_hp $(( $(shhp_hp) + amount )) > /dev/null
+    fi
 }
 
 # Heal to SHHP_MAX_HP and then overheal by AMOUNT.
@@ -124,10 +139,65 @@ function shhp_damage() {
         amount="$1"
     fi
     if (( $(shhp_hp) > 0 )); then
-        SHHP_HP=$(( $(shhp_hp) - amount ))
+        shhp_hp $(( $(shhp_hp) - amount )) > /dev/null
     fi
     if (( $(shhp_hp) < 0 )); then
-        SHHP_HP=0
+        shhp_hp 0 > /dev/null
     fi
 }
 
+# Checks whether the last command executed was an error.
+#
+# Returns 0 if it was an error, 1 if it was not.
+#
+# Usage: shhp_is_error
+function shhp_is_error() {
+    case "$1" in
+        0) return 1 ;;  # No error
+        130) return 1 ;;  # Ctrl-C
+        143) return 1 ;;  # Ctrl-Z
+        *) return 0 ;;  # Error
+    esac
+}
+
+# Checks whether the user should take damage based on the last command executed.
+#
+# The user takes damage if the last command returned an error code, except the
+# ones allowed by shhp_is_error. Repeatedly running the same command does not
+# cause repeated damage.
+#
+# Returns 0 if damage should be taken, 1 if not.
+#
+# Usage: shhp_should_take_damage
+function shhp_should_take_damage() {
+    # The order of these operations is sensitive. Be careful when
+    # editing, because the fc command must count backwards to the interactive
+    # command executed by the user.
+    SHHP_LAST_CODE="$?"
+    SHHP_LAST_COMMAND="$(fc -ln -1 -1 | xargs)"
+    local prev_command="$(cat "${SHHP_TEMP}/prev_command" 2>/dev/null || echo "")"
+
+    if [[ "${prev_command}" == "${SHHP_LAST_COMMAND}:${SHHP_LAST_CODE}" ]]; then
+        return 1  # Repeated command - no damage.
+    fi
+    echo "${SHHP_LAST_COMMAND}:${SHHP_LAST_CODE}" > "${SHHP_TEMP}/prev_command"
+
+    if shhp_is_error "${SHHP_LAST_CODE}"; then
+        return 0  # Take damage
+    fi
+
+    # If the last command was successful, do not take damage.
+    return 1  # No damage taken
+}
+
+function shhp_ps1_hook() {
+    if shhp_should_take_damage; then
+        >&2 printf "You took %s1 damage%s (last command failed: %s)\n" \
+            "$(shhp_color red)" "$(shhp_color reset)" "${SHHP_LAST_COMMAND}"
+        # >&2 echo "Taking damage for last command: ${SHHP_LAST_COMMAND}"
+        shhp_damage
+    fi
+    shhp_print_hp
+}
+
+PS1="\$(shhp_ps1_hook) > "
